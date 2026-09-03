4bfaf2a2e7e8647f4fd7feb440ac1e8ea23aa1f4
Grass-Block <1563575481@qq.com>
2026-09-03 16:37:47 +0800

    Add debug to packet decoding

--- simpnet-base/src/main/java/me/gb2022/simpnet/packet/PacketRegistry.java
+++ simpnet-base/src/main/java/me/gb2022/simpnet/packet/PacketRegistry.java
@@ -12,6 +12,7 @@ public final class PacketRegistry {
     public static final PacketRegistry REGISTRY = new PacketRegistry();
     private final List<Class<? extends Packet>> i2c = new ArrayList<>();
     private final Map<Class<? extends Packet>, Integer> c2i = new HashMap<>();
+    private boolean debug = false;
 
     public PacketRegistry() {
         super();
@@ -20,6 +21,16 @@ public final class PacketRegistry {
     public PacketRegistry(int cap, Consumer<PacketRegistry> initializer) {
         this.allocate(cap);
         initializer.accept(this);
+    }
+
+    public PacketRegistry(int cap, boolean debug, Consumer<PacketRegistry> initializer) {
+        this.setDebug(debug);
+        this.allocate(cap);
+        initializer.accept(this);
+    }
+
+    public void setDebug(boolean debug) {
+        this.debug = debug;
     }
 
     public void allocate(int cap) {
@@ -31,6 +42,10 @@ public final class PacketRegistry {
     public void register(int id, Class<? extends Packet> packet) {
         this.i2c.set(id, packet);
         this.c2i.put(packet, id);
+
+        if (this.debug) {
+            System.out.println("Registered packet: " + id + " -> " + packet.getName() + " ,c2i= " + this.c2i.get(packet));
+        }
     }
 
     public void encode(Packet packet, ByteBuf data) {
@@ -40,8 +55,19 @@ public final class PacketRegistry {
 
             var id = this.c2i.get(packet.getClass());
 
+            if (id == null) {
+                if(this.debug){
+                    System.out.println(this.c2i);
+                }
+                throw new IllegalArgumentException("Invalid packet: " + packet.getClass() + "@" + packet.getClass().hashCode());
+            }
+
             data.writeByte(id);
             packet.write(data);
+
+            if (this.debug) {
+                System.out.println("[encode]" + packet.getClass().getSimpleName() + "->" + id);
+            }
         } catch (Exception e) {
             throw new InvalidPacketFormatException(e);
         }
@@ -52,6 +78,9 @@ public final class PacketRegistry {
             var id = data.readByte();
             var clazz = this.i2c.get(id);
 
+            if (this.debug) {
+                System.out.println("[decode]" + id + "->" + clazz);
+            }
             if (clazz == null) {
                 throw new RuntimeException("Unknown packet id: " + id);
             }
